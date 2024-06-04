6e0f1362a3bbb5110d23315bebfa9ce2f9cb6069
Louise-Xg <78606057+Louise-Xg@users.noreply.github.com>
2024-06-04 10:32:43 +0200

    Update compute_pi_M.m

--- Methods/compute_pi_M.m
+++ Methods/compute_pi_M.m
@@ -1,54 +1,41 @@
+%Created on 2022.
+%author:Louise Xiang
+
 clear all
 close all
 
 path = '/home/xianglo/Bureau/DATA_AMATRICE/DBSCAN_David/new_all_run_files_for_etas/catalog_all/';
 
-%% paramètres issus de "synthetique_modif_v4.m" 
+%% Parameters from previous synthetic tests:
 beta=log(10);    % valeur de b de la G-R * log(10)
-% p=1.1; c=10^(-5); % paramètres loi d'omori
-% alpha=2; % paramètres de productivité
-% gamma=2; % paramètre du kernel en distance (exposant de décroissance avec r)
-% L0=0.5*10^(-2);     % rayon de rupture (en km) pour un magnitude = 0 
-% tmax=365;
-% Lmax=20; Zmax=10; % les + gros séismes (magnitudes entre m1 et m2) sont dans une boite [0 Lmax] x [0 Lmax] x [0 Zmax] (en km)
 m0=-5; % plus petite magnitude possible
-% k=0.001*exp(-alpha*m0);
 m1=5; m2=6; % magnitudes min & max des "gros" chocs principaux (avec K variable). Les séismes m<m1 sont "normaux"%
-% b=beta/(alpha-beta)*k*exp(beta*m0)/(1-exp(-beta*(m1-m0)))*(exp((alpha-beta)*m1)-exp((alpha-beta)*m0))*c^(1-p)/(p-1);
-% s=sprintf('taux de branchement estime = %f (probleme si >1 -> diminuer la valeur de k). ',b); disp(s);         % attention : si >1 alors divergence !
 
-%% charger les données :
-%
+%% 1- Load data:
 load ('/home/xianglo/Bureau/DATA_AMATRICE/DBSCAN_David/new_all_run_files_for_etas/catalog_all/amatrice.mat');
-
 t = t - floor(t(1));
 
-%% couper le catalogue à m0 : 
+%% 2 - Cut-off at m0: 
 J = find(ml >= m0);
-
 t = t(J); ml = ml(J); x = lat(J); y = lon(J); z = z(J);
 
 clear J
 
-%% 5 - mliss : magnitude lissée
+%% 3 - Compute smoothed magnitude mliss:
 L = length(t);
-
 dn = 100;
-
-% Utilisation de "movmean" :
 M = movmean(ml,dn);
-
 I = find(ml > m1);
 
-%% 6 - pi : probabilité de détection
+%% 4 - pi: Earthquake detection probability
 
-%paramètres choisis à partir de synthetique.m :
+%Fixed parameter:
 euler_cst = 0.5772;
 
-%step1 : calcul de mbar lissée (<=> Mmodele) 
+%step1 : Get smoothed magnitude (<=> Mmodele) 
 mbar = M';
 
-%sauvegarder mbar utilisé pour calculer les nvelles valeurs de pi : 
+%Saving mbar used to compute pi later: 
 baseFileName = 'M.dat';
 fullFileName = fullfile(path, baseFileName);
 fid=fopen(fullFileName,'w');
@@ -57,12 +44,12 @@ for n=1:length(mbar)
 end
 fclose(fid);
 
-%step2 : calcul mu, delta, pi :
+%step2: Compute mu, delta, pi:
 mu = mbar - (euler_cst/beta);
 delta0 = exp(-beta*(m0 - mu')); %%%%%%%% NOUVEAU
 pi = (1./delta0).*(1- exp(-delta0)); %%%%%%%% NOUVEAU
 
-%sauvegarde du catalogue après calcul de proba de détection :
+%Save EQ dataframe with their associated pi:
 baseFileName = 'data.dat';
 fullFileName = fullfile(path, baseFileName);
 fid=fopen(fullFileName,'w');
@@ -72,7 +59,7 @@ end
 fclose(fid);
 clear n baseFileName fullFileName
 
-%Sauvegarde de la liste des ml > m1 séismes :
+%Save EQ with ml > m1:
 baseFileName = 'list.dat';
 fullFileName = fullfile(path, baseFileName);
 fid=fopen(fullFileName,'w');
